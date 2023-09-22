e445f3d359295aa6f3561ccf389084c2ca0f552f
gabriel_gc <108274965+gabriel123432@users.noreply.github.com>
2023-09-22 10:59:25 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,7 +8,9 @@ function tocaSom(idElementoAudio){
     let contador = 0;
    // Estrutura de repetição - Ennquanto
     while (contador < listaDeTeclas.length) {
-        listaDeTeclas[contador]. onclick = tocaSomPom;
+        listaDeTeclas[contador]. onclick = function(){
+    tocaSom('#som_tecla_splash')
+        };
         contador = contador + 1;
 
         console.log(contador);
